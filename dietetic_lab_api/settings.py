6bb5ca657cba2c82b984b7611d98ec5019357cbc
Edwige08 <edwige.saves@gmail.com>
2025-09-09 16:31:43 +0200

    fix: cors

--- dietetic_lab_api/settings.py
+++ dietetic_lab_api/settings.py
@@ -73,6 +73,32 @@ MIDDLEWARE = [
 # ]
 CORS_ALLOW_ALL_ORIGINS = True
 
+# Headers autorisés
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+# Méthodes HTTP autorisées
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# Credentials (important pour JWT)
+CORS_ALLOW_CREDENTIALS = True
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
