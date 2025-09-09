cc637382b88217e3520476b0c8329b8adfcb478c
Edwige08 <edwige.saves@gmail.com>
2025-09-09 14:07:46 +0200

    add: dj-database-url

--- dietetic_lab_api/settings.py
+++ dietetic_lab_api/settings.py
@@ -24,6 +24,20 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+if config('DATABASE_URL', default=None):
+    # Production avec DB externe
+    DATABASES = {
+        'default': dj_database_url.parse(config('DATABASE_URL'))
+    }
+else:
+    # Développement local
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
