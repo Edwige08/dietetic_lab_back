c166b7c5fa861444a9a57bc45bef24b9f16def94
Edwige08 <edwige.saves@gmail.com>
2025-10-20 19:59:29 +0200

    add: documentation with drf_spectacular

--- dietetic_lab_api/urls.py
+++ dietetic_lab_api/urls.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 def api_home(request):
     return JsonResponse({
         "message": "Dietetic Lab API",
         "status": "running",
         "version": "1.0",
+        "documentation": "api/docs",
     })
 
 urlpatterns = [
     path('', api_home, name='api_home'),  # Route pour la racine
     path('admin/', admin.site.urls),
     path('api/v1/', include('dietetics.urls')),
+    
+    # Swagger/OpenAPI documentation URLs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
